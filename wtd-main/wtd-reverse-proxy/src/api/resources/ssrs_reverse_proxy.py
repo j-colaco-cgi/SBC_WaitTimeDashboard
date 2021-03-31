e40b363e09edb5720bb54675c40d86525ea0eb71
Rob Lo <rob.lo@cgi.com>
2021-03-31 01:54:40 -0700

    updates to reverse proxy for authentication

--- wtd-main/wtd-reverse-proxy/src/api/resources/ssrs_reverse_proxy.py
+++ wtd-main/wtd-reverse-proxy/src/api/resources/ssrs_reverse_proxy.py
@@ -25,11 +25,10 @@ import requests
 import os
 import jwt
 
+from api.utilities.cors_util import cors_preflight
 from api.auth.auth import jwtmanager
 
 api = Namespace('ssrs', description='Proxy for SSRS embedded reports')
-
-INCLUDE_AUTH = strtobool(os.getenv('INCLUDE_AUTH', 'True'))
 
 SSRS_SERVER = os.getenv('SSRS_SERVER')
 SSRS_BASE_URI = os.getenv('SSRS_BASE_URI')
@@ -40,7 +39,8 @@ ORIGIN = f'http://{SSRS_SERVER}'
 SITE_NAME = f'http://{SSRS_SERVER}/{SSRS_BASE_URI}'
 
 PREFIX = 'Bearer '
-SSRS_ACCESS_GROUP = os.getenv('SSRS_ACCESS_GROUP')
+COOKIE_PREFIX = 'wtd-rp='
+SSRS_ACCESS_GROUP = os.getenv('SSRS_ACCESS_GROUP', 'SBC Staff')
 
 
 #  http://localhost:5000/ReportServer/Pages/ReportViewer.aspx?%2FProject-COVID_SI%2FCOVID%20SI%20Plan%20-%20Director%20Dashboard&rs%3AParameterLanguage=en-CA
@@ -56,78 +56,58 @@ def get_token(header):
         raise ValueError('Invalid token')
     return header[len(PREFIX):]
 
-@api.route('/<path:path>',methods=['GET','POST'])
+def get_token_from_cookie(headers):
+    if headers and 'Cookie' in headers:
+        header = headers['Cookie']
+        if header:
+            cookies = header.split(';')
+            if cookies:
+                for cookie in cookies:
+                    strippedCookie = cookie.strip()
+                    if strippedCookie.startswith(COOKIE_PREFIX):
+                        return strippedCookie[len(COOKIE_PREFIX):]
+    return None
+
+@cors_preflight('GET,POST,OPTIONS')
+@api.route('/<path:path>',methods=['GET','POST','OPTIONS'])
 class SSRSProxy(Resource):
 
     @cors.crossdomain(origin='*')
     # @jwtmanager.requires_auth
     def get(self, path):
-        if INCLUDE_AUTH:
-            token = get_token(request.headers['Authorization'])
+        if path and path.lower() == 'pages/reportviewer.aspx':
+            # Just check the if the inital request has a token. no need to check all the javascript and css files that come back
+            token = get_token_from_cookie(request.headers)
+            if not token:
+                return {'error': 'Missing token'}, 401
             decoded = jwt.decode(token, verify=False)
             groups = decoded['groups']
-            if SSRS_ACCESS_GROUP in groups:
-                print(f'USERNAME: {SSRS_SYSTEM_USER}')
-                query = request.query_string.decode()
-                # print(f'----> GET  REDIRECT:   {SITE_NAME}/{path}?{query}')
-                #resp = requests.get(f'{SITE_NAME}/{path}?{query}', headers=self.parse_headers())
-                resp = requests.get(f'{SITE_NAME}/{path}?{query}', headers=self.parse_headers(), auth=HttpNtlmAuth(SSRS_SYSTEM_USER, SSRS_SYSTEM_CODE))
+            print(f'Groups are: {groups}')
+            if SSRS_ACCESS_GROUP not in groups:
+                return {'error': 'Unsufficient permissions'}, 401
+        query = request.query_string.decode()
+        resp = requests.get(f'{SITE_NAME}/{path}?{query}', headers=self.parse_headers(), auth=HttpNtlmAuth(SSRS_SYSTEM_USER, SSRS_SYSTEM_CODE))
 
-                response = Response(resp.content, resp.status_code)
+        response = Response(resp.content, resp.status_code)
 
-                if resp.headers.get('Content-Type') != None:
-                    response.headers.set('Content-Type', resp.headers.get('Content-Type'))
-                return response
-            else:
-                return {'error': 'Unsufficient keycloak group permissions'}, 401
-        else:
-            print(f'USERNAME: NO AUTH {SSRS_SYSTEM_USER}')
-            query = request.query_string.decode()
-            # print(f'----> GET  REDIRECT:   {SITE_NAME}/{path}?{query}')
-            #resp = requests.get(f'{SITE_NAME}/{path}?{query}', headers=self.parse_headers())
-            resp = requests.get(f'{SITE_NAME}/{path}?{query}', headers=self.parse_headers(), auth=HttpNtlmAuth(SSRS_SYSTEM_USER, SSRS_SYSTEM_CODE))
-
-            response = Response(resp.content, resp.status_code)
-
-            if resp.headers.get('Content-Type') != None:
-                response.headers.set('Content-Type', resp.headers.get('Content-Type'))
-            return response
+        if resp.headers.get('Content-Type') != None:
+            response.headers.set('Content-Type', resp.headers.get('Content-Type'))
+        return response
 
     @cors.crossdomain(origin='*')
     # @jwtmanager.requires_auth
     def post(self, path):
-        if INCLUDE_AUTH:
-            token = get_token(request.headers['Authorization'])
-            decoded = jwt.decode(token, verify=False)
-            groups = decoded['groups']
-            if SSRS_ACCESS_GROUP in groups:
-                query = request.query_string.decode()
-                payload = request.get_data()
-                # print(f'----> POST REDIRECT:   {SITE_NAME}/{path}?{query}')
-                
-                # TODO Maybe payload will be different for different queries
-                textpayload = f'{payload}'
-                textpayload = textpayload[2:-1]
+        query = request.query_string.decode()
+        payload = request.get_data()
+        
+        # TODO Maybe payload will be different for different queries
+        textpayload = f'{payload}'
+        textpayload = textpayload[2:-1]
 
-                resp = requests.post(f'{SITE_NAME}/{path}?{query}',data=textpayload, headers=self.parse_headers(), auth=HttpNtlmAuth(SSRS_SYSTEM_USER, SSRS_SYSTEM_CODE))
-                
-                response = Response(resp.content, resp.status_code)
-                return response
-            else:
-                return {'error': 'Unsufficient keycloak group permissions'}, 401
-        else:
-            query = request.query_string.decode()
-            payload = request.get_data()
-            # print(f'----> POST REDIRECT:   {SITE_NAME}/{path}?{query}')
-            
-            # TODO Maybe payload will be different for different queries
-            textpayload = f'{payload}'
-            textpayload = textpayload[2:-1]
-
-            resp = requests.post(f'{SITE_NAME}/{path}?{query}',data=textpayload, headers=self.parse_headers(), auth=HttpNtlmAuth(SSRS_SYSTEM_USER, SSRS_SYSTEM_CODE))
-            
-            response = Response(resp.content, resp.status_code)
-            return response
+        resp = requests.post(f'{SITE_NAME}/{path}?{query}',data=textpayload, headers=self.parse_headers(), auth=HttpNtlmAuth(SSRS_SYSTEM_USER, SSRS_SYSTEM_CODE))
+        
+        response = Response(resp.content, resp.status_code)
+        return response
 
     def parse_headers(self):
         excluded_headers = ['referer', 'host', 'origin']
@@ -136,7 +116,7 @@ class SSRSProxy(Resource):
             new_headers[name] = value
         if request.headers.get('Referer') != None:
             original_referrer = request.headers.get('Referer')
-            print(f'Original Referrer: {original_referrer}, SSRS_BASE: {SSRS_BASE_URI}')
+            # print(f'Original Referrer: {original_referrer}, SSRS_BASE: {SSRS_BASE_URI}')
             if SSRS_BASE_URI in original_referrer:
                 index = original_referrer.index(SSRS_BASE_URI)
                 existing_uri = original_referrer[index + len(SSRS_BASE_URI) + 1:]
